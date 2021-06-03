cd0119d7a079ca51f61e250961d27eb1a733cf4e
amc1999 <29126882+amc1999@users.noreply.github.com>
2021-06-03 00:50:30 -0400

    1.make content of top bin folder identical for all platforms 2.CentOS: cleanup outdated Docker sources

--- ompp-build-win/build-zip.bat
+++ ompp-build-win/build-zip.bat
@@ -115,12 +115,13 @@ call :rcopy_files ^
 
 REM copy bin directory executables
 
+call :rcopy_files %DEPLOY_DIR%\bin bin "omc.exe omc.ini omc.message.ini sqlite3.exe"
+call :rcopy_files %DEPLOY_DIR%\bin bin "ompp_ui.bat ompp_ui_linux.sh ompp_ui_macOS.command start_oms.sh README_win.txt"
+
 call :rcopy_files ^
   %DEPLOY_DIR%\bin ^
   bin ^
-  "omc.exe omc.message.ini test_models.exe ompp_create_scex.exe ompp_export_csv.exe ompp_export_excel.exe modgen_export_csv.exe patch_modgen11_outputs.exe patch_modgen12_outputs.exe patch_modgen12.1_outputs.exe sqlite3.exe"
-
-call :rcopy_files %DEPLOY_DIR%\bin  bin "README_win.txt ompp_ui.bat"
+  "test_models.exe ompp_create_scex.exe ompp_export_csv.exe ompp_export_excel.exe modgen_export_csv.exe patch_modgen11_outputs.exe patch_modgen12_outputs.exe patch_modgen12.1_outputs.exe"
 
 REM copy Go bin executables and source code
 
